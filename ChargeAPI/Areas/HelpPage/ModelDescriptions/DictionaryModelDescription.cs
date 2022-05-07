a8cbac3eb3380352fb62c52655e16a46bb688b8c
Katlego Mohale <u19194588@tuks.co.za>
2022-05-07 11:50:26 +0200

    Initial Commit

---
+++ ChargeAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ChargeAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ChargeAPI/Models/UserType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChargeAPI.Models
+{
+    public class UserType
+    {
+        public int UserTypeID { get; set; }
+        public string UserTypeName { get; set; }
+        public string UserTypeDescription { get; set; }
+    }
+}
